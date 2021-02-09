31efa7e2809a919f6128ab2648e215f223a8a100
Salvatore Mazzarino <dev@mazzarino.cz>
2021-02-09 12:00:07 +0100

    Fix CLI pass env
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
 	"github.com/mazzy89/containervmm/pkg/api"
@@ -28,141 +29,157 @@ import (
 	"github.com/mazzy89/containervmm/pkg/distro"
 	"github.com/mazzy89/containervmm/pkg/hypervisor"
 	"github.com/mazzy89/containervmm/pkg/network"
-
-	"github.com/spf13/cobra"
 )
 
 const targetName = "containervmm"
 
-type runOptions struct {
-	guestName string
+var c = viper.New()
 
-	guestMemory          string
-	guestCPUs            string
-	guestRootDiskSize    string
-	guestAdditionalDisks []string
-	guestHostVolumes     []string
+var (
+	cfgGuestName            = "guest-name"
+	cfgGuestMemory          = "guest-memory"
+	cfgGuestCPUs            = "guest-cpus"
+	cfgGuestRootDiskSize    = "guest-root-disk-size"
+	cfgGuestAdditionalDisks = "guest-additional-disks"
+	cfgGuestHostVolumes     = "guest-host-volumes"
 
-	flatcarChannel string
-	flatcarVersion string
+	cfgFlatcarChannel  = "flatcar-channel"
+	cfgFlatcarVersion  = "flatcar-version"
+	cfgFlatcarIgnition = "flatcar-ignition"
 
-	// path where the Ignition config is stored
-	flatcarIgnitionConfig string
+	cfgDebug        = "debug"
+	cfgSanityChecks = "sanity-checks"
+)
 
-	sanityChecks bool
-	debug        bool
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:     fmt.Sprintf("%s [options]", targetName),
+	Short:   "Container Virtual Machine Manager",
+	Long:    `Container Virtual Machine Manager spins up a Virtual Machine inside a container`,
+	Example: fmt.Sprintf("%s --flatcar-version=2605.6.0", targetName),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// create Guest API object
+		guest := api.Guest{
+			Name:   c.GetString(cfgGuestName),
+			CPUs:   c.GetString(cfgGuestCPUs),
+			Memory: c.GetString(cfgGuestMemory),
+		}
+
+		kernel, initrd, err := distro.DownloadImages(c.GetString(cfgFlatcarChannel), c.GetString(cfgFlatcarVersion), c.GetBool(cfgSanityChecks))
+		if err != nil {
+			return fmt.Errorf("an error occurred during the download of Flatcar %s %s images: %v",
+				c.GetString(cfgFlatcarChannel), c.GetString(cfgFlatcarVersion), err)
+		}
+
+		// set kernel and initrd downloaded
+		guest.OS.Kernel = kernel
+		guest.OS.Initrd = initrd
+
+		// set Ignition Config
+		guest.OS.IgnitionConfig = c.GetString(cfgFlatcarIgnition)
+
+		// Setup networking inside of the container, return the available interfaces
+		dhcpIfaces, err := network.SetupInterfaces(&guest)
+		if err != nil {
+			return fmt.Errorf("an error occured during the the setup of the network: %v", err)
+		}
+
+		// Serve DHCP requests for those interfaces
+		// This function returns the available IP addresses that are being
+		// served over DHCP now
+		if err = network.StartDHCPServers(guest, dhcpIfaces); err != nil {
+			return fmt.Errorf("an error occured during the start of the DHCP servers: %v", err)
+		}
+
+		// create rootfs and other additional volumes
+		gDisks := guest.Disks
+		gDisks = append(gDisks, api.Disk{
+			ID:     "rootfs",
+			Size:   c.GetString(cfgGuestRootDiskSize),
+			IsRoot: true,
+		})
+
+		for _, gd := range c.GetStringSlice(cfgGuestAdditionalDisks) {
+			id, size := parseStringSliceFlag(gd)
+
+			gDisks = append(gDisks, api.Disk{
+				ID:     id,
+				Size:   size,
+				IsRoot: false,
+			})
+		}
+
+		if err := disk.CreateDisks(&guest); err != nil {
+			return fmt.Errorf("an error occured during the creation of disks: %v", err)
+		}
+
+		for _, gv := range c.GetStringSlice(cfgGuestHostVolumes) {
+			mountTag, hostPath := parseStringSliceFlag(gv)
+
+			guest.HostVolumes = append(guest.HostVolumes, api.HostVolume{
+				MountTag: mountTag,
+				HostPath: hostPath,
+			})
+		}
+
+		// execute QEMU
+		if err = hypervisor.ExecuteQEMU(guest); err != nil {
+			return fmt.Errorf("an error occured during the execution of QEMU: %v", err)
+		}
+
+		return nil
+	},
 }
 
-var opts runOptions
-
 func Execute() error {
-	// rootCmd represents the base command when called without any subcommands
-	var rootCmd = &cobra.Command{
-		Use:   targetName,
-		Short: "Container Virtual Machine Manager",
-		Long:  `Container Virtual Machine Manager spins up a Virtual Machine inside a container`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// create Guest API object
-			guest := api.Guest{
-				Name:   opts.guestName,
-				CPUs:   opts.guestCPUs,
-				Memory: opts.guestMemory,
-			}
-
-			kernel, initrd, err := distro.DownloadImages(opts.flatcarChannel, opts.flatcarVersion, opts.sanityChecks)
-			if err != nil {
-				return fmt.Errorf("an error occurred during the download of Flatcar %s %s images: %v", opts.flatcarChannel, opts.flatcarVersion, err)
-			}
-
-			// set kernel and initrd downloaded
-			guest.OS.Kernel = kernel
-			guest.OS.Initrd = initrd
-			// set Ignition Config
-			guest.OS.IgnitionConfig = opts.flatcarIgnitionConfig
-
-			// Setup networking inside of the container, return the available interfaces
-			dhcpIfaces, err := network.SetupInterfaces(&guest)
-			if err != nil {
-				return fmt.Errorf("an error occured during the the setup of the network: %v", err)
-			}
-
-			// Serve DHCP requests for those interfaces
-			// This function returns the available IP addresses that are being
-			// served over DHCP now
-			if err = network.StartDHCPServers(guest, dhcpIfaces); err != nil {
-				return fmt.Errorf("an error occured during the start of the DHCP servers: %v", err)
-			}
-
-			// create rootfs and other additional volumes
-			gDisks := guest.Disks
-			gDisks = append(gDisks, api.Disk{
-				ID:     "rootfs",
-				Size:   opts.guestRootDiskSize,
-				IsRoot: true,
-			})
-
-			if len(opts.guestAdditionalDisks) > 0 {
-				for _, gd := range opts.guestAdditionalDisks {
-					s := strings.Split(gd, ":")
-					id, size := s[0], s[1]
-
-					gDisks = append(gDisks, api.Disk{
-						ID:     id,
-						Size:   size,
-						IsRoot: false,
-					})
-				}
-			}
-
-			if err := disk.CreateDisks(&guest); err != nil {
-				return fmt.Errorf("an error occured during the creation of disks: %v", err)
-			}
-
-			if len(opts.guestHostVolumes) > 0 {
-				for _, gv := range opts.guestHostVolumes {
-					s := strings.Split(gv, ":")
-					mountTag, hostPath := s[0], s[1]
-
-					guest.HostVolumes = append(guest.HostVolumes, api.HostVolume{
-						MountTag: mountTag,
-						HostPath: hostPath,
-					})
-				}
-			}
-
-			// execute QEMU
-			if err = hypervisor.ExecuteQEMU(guest); err != nil {
-				return fmt.Errorf("an error occured during the execution of QEMU: %v", err)
-			}
-
-			return nil
-		},
-	}
-
-	flags := rootCmd.PersistentFlags()
-	flags.BoolVarP(&opts.debug, "debug", "", false, "enable debug")
-	flags.StringVarP(&opts.guestName, "guest-name", "", "flatcar_production_qemu", "uest name")
-	flags.StringVarP(&opts.guestMemory, "guest-memory", "m", "1024M", "guest memory")
-	flags.StringVarP(&opts.guestCPUs, "guest-cpus", "c", "1", "guest cpus")
-
-	flags.StringVarP(&opts.guestRootDiskSize, "guest-root-disk-size", "s", "20G", "guest root disk size")
-	flags.StringSliceVarP(&opts.guestAdditionalDisks, "guest-additional-disks", "d", []string{}, "comma-separated list of guest additional disks to mount (i.e. \"dockerfs:20GB,kubeletfs:100GB\")")
-	flags.StringSliceVarP(&opts.guestHostVolumes, "guest-host-volumes", "", []string{}, "comma-separated list of guest host volumes (i.e. \"datashare:/usr/data\")")
-
-	flags.StringVarP(&opts.flatcarChannel, "flatcar-channel", "", "stable", "flatcar channel")
-	flags.StringVarP(&opts.flatcarVersion, "flatcar-version", "", "", "flatcar version")
-	rootCmd.MarkPersistentFlagRequired("flatcar-version")
-	flags.StringVarP(&opts.flatcarIgnitionConfig, "flatcar-ignition", "", "", "path of the Ignition config")
-
-	flags.BoolVarP(&opts.sanityChecks, "sanity-checks", "", true, "run sanity checks (GPG verification of images)")
-
 	return rootCmd.Execute()
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	flags := rootCmd.PersistentFlags()
+
+	flags.Bool(cfgDebug, false, "enable debug")
+	_ = c.BindPFlag(cfgDebug, flags.Lookup(cfgDebug))
+
+	flags.String(cfgGuestName, "flatcar_production_qemu", "guest name")
+	_ = c.BindPFlag(cfgGuestName, flags.Lookup(cfgGuestName))
+
+	flags.String(cfgGuestMemory, "1024M", "guest memory")
+	_ = c.BindPFlag(cfgGuestMemory, flags.Lookup(cfgGuestMemory))
+
+	flags.String(cfgGuestCPUs, "1", "guest cpus")
+	_ = c.BindPFlag(cfgGuestCPUs, flags.Lookup(cfgGuestCPUs))
+
+	flags.String(cfgGuestRootDiskSize, "20G", "guest root disk size")
+	_ = c.BindPFlag(cfgGuestRootDiskSize, flags.Lookup(cfgGuestRootDiskSize))
+
+	flags.StringSlice(cfgGuestAdditionalDisks, []string{}, "guest additional disk to mount (i.e. \"dockerfs:20GB\")")
+	_ = c.BindPFlag(cfgGuestAdditionalDisks, rootCmd.PersistentFlags().Lookup(cfgGuestAdditionalDisks))
+	flags.StringSlice(cfgGuestHostVolumes, []string{}, "guest host volume (i.e. \"datashare:/usr/data\")")
+	_ = c.BindPFlag(cfgGuestHostVolumes, flags.Lookup(cfgGuestHostVolumes))
+
+	flags.String(cfgFlatcarChannel, "stable", "flatcar channel (i.e. stable, beta, alpha, edge)")
+	_ = c.BindPFlag(cfgFlatcarChannel, flags.Lookup(cfgFlatcarChannel))
+	flags.String(cfgFlatcarVersion, "", "flatcar version")
+	_ = c.BindPFlag(cfgFlatcarVersion, flags.Lookup(cfgFlatcarVersion))
+	flags.String(cfgFlatcarIgnition, "", "path of the Ignition config")
+	_ = c.BindPFlag(cfgFlatcarIgnition, flags.Lookup(cfgFlatcarIgnition))
+
+	flags.Bool(cfgSanityChecks, true, "run sanity checks (GPG verification of images)")
 }
 
 func initConfig() {
-	viper.AutomaticEnv() // read in environment variables that match
+	c.SetEnvPrefix(targetName)
+	replacer := strings.NewReplacer("-", "_")
+	c.SetEnvKeyReplacer(replacer)
+
+	c.AutomaticEnv() // read in environment variables that match
+}
+
+func parseStringSliceFlag(input string) (string, string) {
+	s := strings.Split(input, ":")
+
+	return s[0], s[1]
 }
